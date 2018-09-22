0aa02cd8b55c9ab3dc89d884c0ff03d2d6f797f7
Metre-Ray <dgary@tut.by>
2018-09-22 11:49:09 +0300

    modified index

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  var p = preferences,
+      returns = 0;
+  for (var i = 0; i < p.length; i++) {
+    if (p[i] !== 0 && p[p[i] - 1] !== p[i]) {
+        if (p[p[p[i] - 1] - 1] === i + 1) {
+          returns += 1;
+        }
+    }
+  }
+  return returns / 3;
+}
